43a492e27d17ab015fe692d9c0080bdcd462d3e3
ayse <atrk61@gmail.com>
2021-03-13 20:26:06 +0100

    ilk commit demo projesi icin

---
+++ src/ayseDemo01/Ayse01_Class.java
@@ -0,0 +1,11 @@
+package ayseDemo01;
+
+public class Ayse01_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Local branch den remote icin ilk mesaj Hello Word 01");
+	}
+
+}
